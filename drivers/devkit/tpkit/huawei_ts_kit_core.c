4754283caaa56ad10fad5ba44c4f5c4d7c8e4a8e
KernelSU Port <port@local>
2026-08-09 17:40:45 +0800

    fix: ipv6_stub dst_lookup_flow (addrconf VTR), tpkit include

--- drivers/devkit/tpkit/huawei_ts_kit_core.c
+++ drivers/devkit/tpkit/huawei_ts_kit_core.c
@@ -34,7 +34,7 @@
 #endif
 #include "huawei_ts_kit.h"
 #include "huawei_ts_kit_misc_dev.h"
-#include <tpkit_platform_adapter.h>
+#include "tpkit_platform_adapter.h"
 #include <huawei_ts_kit_api.h>
 #include <huawei_ts_kit_algo.h>
 #include <linux/hwspinlock.h>

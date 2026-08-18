9a6cb010f1a988cb8be8bda6891808aabe2de08c
KernelSU Port <port@local>
2026-08-18 23:55:55 +0800

    fix: usb.h declare enable/disable_usb2_hardware_lpm (align VTR)

--- drivers/usb/core/usb.h
+++ drivers/usb/core/usb.h
@@ -84,6 +84,8 @@ extern int usb_remote_wakeup(struct usb_device *dev);
 extern int usb_runtime_suspend(struct device *dev);
 extern int usb_runtime_resume(struct device *dev);
 extern int usb_runtime_idle(struct device *dev);
+extern int usb_enable_usb2_hardware_lpm(struct usb_device *udev);
+extern int usb_disable_usb2_hardware_lpm(struct usb_device *udev);
 extern int usb_set_usb2_hardware_lpm(struct usb_device *udev, int enable);
 
 #else
